969ffa1a155db967884724d33d3577c5fd97c6a4
cice <a>
2017-02-01 18:16:35 +0100

    Generados 3 rectángulos con tres colores. el amarillo y el verde están dentro del rojo

--- Vistas/ViewController.m
+++ Vistas/ViewController.m
@@ -16,7 +16,36 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    // Extraemos dimensiones de la vista padre
+    CGRect rectVistaPadre = self.view.bounds;
+    NSLog(@"Vista padre mide: %f x %f", rectVistaPadre.size.width, rectVistaPadre.size.height);
+    
+    // Creamos una vista
+    UIView *vistaRoja = [[UIView alloc] initWithFrame:CGRectMake(10, 10, rectVistaPadre.size.width-20, rectVistaPadre.size.height-20)];
+    vistaRoja.backgroundColor = [UIColor redColor];
+    
+    // Metodo para añadir la vista al Controlador la vista raiz
+    [self.view addSubview:vistaRoja];
+    
+    
+    // creamos otra vista amarilla
+    UIView *vistaAmarilla = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    vistaAmarilla.backgroundColor = [UIColor yellowColor];
+    
+    // Añadimos la vista amarilla dentro de la vista roja
+    [vistaRoja addSubview:vistaAmarilla];
+    
+    CGRect rectVistaRoja = vistaRoja.bounds;
+    
+    // creamos otra vista
+    UIView *vistaVerde = [[UIView alloc] initWithFrame:CGRectMake(0, rectVistaRoja.size.height /2, rectVistaRoja.size.width, rectVistaRoja.size.height/2)];
+    vistaVerde.backgroundColor = [UIColor greenColor];
+    
+    // Añadimos la vista amarilla dentro de la vista roja
+    [vistaRoja addSubview:vistaVerde];
+    
+    
 }
 
 
